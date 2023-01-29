2750ead74f3c318a047e97d945b7137059d67c90
hekxo <raminsagandykov@gmail.com>
2023-01-29 14:40:04 +0600

    Changed comment from Nourbol

--- src/main/java/kz/carmarketplace/Main.java
+++ src/main/java/kz/carmarketplace/Main.java
@@ -2,7 +2,7 @@ package kz.carmarketplace;
 
 public class Main {
 
-    // Some changes.
+    // Фигня.
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
